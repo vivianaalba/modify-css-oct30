3079c3ea2d9cc7cf7878e5f596fbccb45b1e6ba8
Viviana Alba <141361486+vivianaalba@users.noreply.github.com>
2023-11-01 13:42:20 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// Example 1: Change the background color of this div to yellow
+let example1 = document.getElementById("example1");
+example1.style.backgroundColor = "yellow";
+
+// Example 2: Increase the font size of this paragraph to 24px
+let example2 = document.getElementById("example2");
+example2.style.fontSize = "24px";
+
+// Example 3: Make the text color of this heading red
+let example3 = document.querySelector(".example3");
+example3.style.color = "red";
+
+// Example 4: Change the border of this button to a solid black border
+let example4 = document.getElementById("example4");
+example4.style.border = "3px solid black";
+
+// Example 5: Set a new background image for this section
+let example5 = document.getElementById("example5");
+example5.style.backgroundImage = 'url("https://images.unsplash.com/photo-1698764714629-861fc31ed3cd?auto=format&fit=crop&q=80&w=2940&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")';
+
+// Example 6: Hide this paragraph by setting its display property to 'none'
+let example6 = document.getElementById("example6");
+example6.style.display = "none";
+
+// Example 7: Change the font color of this list to green
+let example7 = document.querySelector(".example7");
+example7.style.color = "green";
